427023c791fb2d8fe7be8b030f6a2d90f5de0a4d
neelam <null.0990.null@gmail.com>
2023-05-17 21:45:37 +0530

    done

--- src/Page.js
+++ src/Page.js
@@ -3,13 +3,11 @@ import { default as BookIcon } from "./assets/book-icon.svg";
 import { default as ClockIcon } from "./assets/clock-icon.svg";
 import { default as CapIcon } from "./assets/cap-icon.svg";
 import { default as AdIcon } from "./assets/ad-icon.svg";
-import { default as NoTime } from "./assets/no-time.svg";
 import { default as TvIcon } from "./assets/tv-icon.svg";
-import { default as SearchIcon } from "./assets/search.svg";
-import { default as Razorpay } from "./assets/razorpay.svg";
 import { default as Vicon } from "./assets/v.svg";
-
-import { Checkbox, Button } from "@mantine/core";
+import { default as SearchIcon } from "./assets/search.svg";
+import { Button } from "@mantine/core";
+import SubscriptionCard from "./components/SubscriptionCard/SubscriptionCard";
 
 const Page = () => {
   return (
@@ -38,7 +36,7 @@ const Page = () => {
             fz={"lg"}
             radius="xl"
             px={"3.3rem"}
-            h="3rem"
+            // h="3rem"
             variant="gradient"
             gradient={{ from: "#0048ff", to: "#0096ff", deg: 105 }}
           >
@@ -54,8 +52,6 @@ const Page = () => {
               <span
                 style={{
                   textDecoration: "line-through red 6px",
-                  // textDecorationColor: "red",
-                  // textDecorationThickness: "10px",
                 }}
               >
                 18,500
@@ -66,12 +62,7 @@ const Page = () => {
             </div>
             <div className="keypoints" style={{ fontSize: "1.8rem" }}>
               <div className="keypoint">
-                <img
-                  src={BookIcon}
-                  alt="book-icon"
-                  // height="40px"
-                  // style={{ marginRight: "1rem" }}
-                />
+                <img src={BookIcon} alt="book-icon" />
 
                 <p>
                   <span>100+</span> Job relevant courses
@@ -133,143 +124,7 @@ const Page = () => {
           </div>
         </div>
         <div className="r-main">
-          <div className="card">
-            <div className="progress-bar">
-              <div className="signup-container">
-                <div className="sign-up">1</div>
-                <span style={{ fontSize: "0.8rem" }}>Sign Up</span>
-              </div>
-              <div className="subscribe-container">
-                <div className="subscribe">2</div>
-                <span style={{ fontSize: "0.8rem" }}>Subscribe</span>
-              </div>
-            </div>
-            <p className="form-header">Select your subcription plan</p>
-            <form className="form">
-              <div
-                className="input-container"
-                style={{ background: "#E7E7E7" }}
-              >
-                <div>
-                  <div
-                    className="tag"
-                    style={{
-                      background: "#F77171",
-                      border: "#F77171 2px solid",
-                    }}
-                  >
-                    Offer expired
-                  </div>
-                  <Checkbox
-                    className="checkbox"
-                    label="12 Months Subscription"
-                    radius="xl"
-                    size="md"
-                    fw={"500"}
-                    disabled
-                  />
-                </div>
-                <div className="text-container">
-                  <span>Total ₹179</span>
-                  <span> ₹15 /mo</span>
-                </div>
-              </div>
-              <div
-                className="input-container"
-                style={{ background: "#D7EDDD", border: "2px solid #47BA68" }}
-              >
-                <div>
-                  <div
-                    className="tag"
-                    style={{
-                      background: "#47BA68",
-                      border: "#47BA68 2px solid",
-                    }}
-                  >
-                    Recommended
-                  </div>
-                  <Checkbox
-                    color="green"
-                    className="checkbox"
-                    label="12 Months Subscription"
-                    radius="xl"
-                    size="md"
-                    fw={"500"}
-                  />
-                </div>
-                <div className="text-container">
-                  <span>Total ₹179</span>
-                  <span> ₹15 /mo</span>
-                </div>
-              </div>
-              <div className="input-container">
-                <Checkbox
-                  className="checkbox"
-                  label="6 Months Subscription"
-                  radius="xl"
-                  size="md"
-                  fw={"500"}
-                />
-                <div className="text-container">
-                  <span>Total ₹179</span>
-                  <span> ₹15 /mo</span>
-                </div>
-              </div>
-              <div className="input-container">
-                <Checkbox
-                  className="checkbox"
-                  label="3 Months Subscription"
-                  radius="xl"
-                  size="md"
-                  fw={"500"}
-                />
-                <div className="text-container">
-                  <span style={{ display: "block" }}>Total ₹179</span>
-                  <span> ₹15 /mo</span>
-                </div>
-              </div>
-              <hr style={{ marginTop: "1.5rem" }} />
-              <div className="summary-container">
-                <div className="st-container">
-                  <p>Subscription Fee</p>
-                  <p>₹1500</p>
-                </div>
-                <div className="alert">
-                  <div className="offer">
-                    <p>Limited time offer</p>
-                    <p>- ₹18,401</p>
-                  </div>
-                  <div className="alert-body">
-                    <img src={NoTime} alt="search-icon"></img>
-                    <p style={{ paddingLeft: "5px" }}>
-                      Offer valid till 25th March 2023{" "}
-                    </p>
-                  </div>
-                </div>
-                <div className="total-price">
-                  <p>Total (Incl. of 18% GST)</p>
-                  <p>₹149</p>
-                </div>
-                <div className="btn-container">
-                  <Button
-                    variant="outline"
-                    color="red"
-                    size="md"
-                    w="48%"
-                    h="3rem"
-                  >
-                    CANCEL
-                  </Button>
-                  <Button size="md" w="48%" h="3rem">
-                    PROCEED TO PAY{" "}
-                  </Button>
-                </div>
-                <div style={{ display: "flex", marginTop: "1.2rem" }}>
-                  <img src={Razorpay} alt="razorpay"></img>
-                </div>
-              </div>
-            </form>
-          </div>
+          <SubscriptionCard />
         </div>
       </div>
     </div>
---
+++ src/components/SubscriptionCard/SubscriptionCard.js
@@ -0,0 +1,260 @@
+import React from "react";
+import "./SubscriptionCard.css";
+
+import { default as NoTime } from "../../assets/no-time.svg";
+
+import { default as Razorpay } from "../../assets/razorpay.svg";
+
+import { Checkbox, Button } from "@mantine/core";
+import { useState } from "react";
+
+export default function SubscriptionCard() {
+  const card = [
+    {
+      id: 1,
+      text: "12 Months Subscription",
+      expired: true,
+      recommend: false,
+      total_price: 99,
+    },
+    {
+      id: 2,
+      text: "12 Months Subscription",
+      expired: false,
+      recommend: true,
+      total_price: 179,
+    },
+    {
+      id: 3,
+      text: "6 Months Subscription",
+      expired: false,
+      recommend: false,
+      total_price: 149,
+    },
+    {
+      id: 4,
+      text: "3 Months Subscription",
+      expired: false,
+      recommend: false,
+      total_price: 99,
+    },
+  ];
+
+  const discount_price = 50;
+
+  const [seletedSubscription, setSeletedSubscription] = useState(-1);
+  const [subscriptionPrice, setSubscriptionPrice] = useState(null);
+
+  const onChangeHandler = (id) => {
+    setSeletedSubscription(id);
+    setSubscriptionPrice(card[id - 1].total_price);
+  };
+  return (
+    <div className="card">
+      <div className="progress-bar">
+        <div className="signup-container">
+          <div className="sign-up">1</div>
+          <span style={{ fontSize: "0.8rem" }}>Sign Up</span>
+        </div>
+        <div className="subscribe-container">
+          <div className="subscribe">2</div>
+          <span style={{ fontSize: "0.8rem" }}>Subscribe</span>
+        </div>
+      </div>
+      <p className="form-header">Select your subcription plan</p>
+      <form className="form">
+        {/* <div
+      className="input-container"
+      style={{ background: "#E7E7E7" }}
+    > */}
+        {card.map((ele) => (
+          <div
+            className="input-container"
+            // style={{
+            //   background: `${ele.expired} ? "#E7E7E7" : ${ele.recommend} ? "#47BA68": "none"`,
+            // }}
+            style={{
+              background: ele.expired
+                ? "#E7E7E7"
+                : seletedSubscription === ele.id
+                ? "#D7EDDD"
+                : "none",
+            }}
+          >
+            <div>
+              {ele.expired ? (
+                <div
+                  className="tag"
+                  style={{
+                    background: "#F77171",
+                    border: "#F77171 2px solid",
+                    cursor: "pointer",
+                  }}
+                >
+                  Offer expired
+                </div>
+              ) : null}{" "}
+              {ele.recommend ? (
+                <div
+                  className="tag"
+                  style={{
+                    background: "#47BA68",
+                    border: "#47BA68 2px solid",
+                  }}
+                >
+                  Recommended
+                </div>
+              ) : null}
+              <Checkbox
+                className="checkbox"
+                color={seletedSubscription === ele.id ? "green" : ""}
+                label={ele.text}
+                radius="xl"
+                size="md"
+                fw={"500"}
+                disabled={ele.expired}
+                value={seletedSubscription}
+                onChange={() => onChangeHandler(ele.id)}
+                checked={seletedSubscription === ele.id}
+              />
+            </div>
+            <div className="text-container">
+              <span>
+                Total{" "}
+                <span style={{ fontSize: "0.9rem", fontWeight: "500" }}>
+                  ₹{ele.total_price}
+                </span>
+              </span>
+              <span>
+                {" "}
+                ₹{(ele.total_price / 12).toFixed()}{" "}
+                <span style={{ color: "#BEBEBE" }}>/mo</span>
+              </span>
+            </div>
+          </div>
+        ))}
+        {/* <div>
+        <div
+          className="tag"
+          style={{
+            background: "#F77171",
+            border: "#F77171 2px solid",
+          }}
+        >
+          Offer expired
+        </div>
+        <Checkbox
+          className="checkbox"
+          label="12 Months Subscription"
+          radius="xl"
+          size="md"
+          fw={"500"}
+          disabled
+        />
+      </div>
+      <div className="text-container">
+        <span>Total ₹179</span>
+        <span> ₹15 /mo</span>
+      </div>
+    </div>
+    <div
+      className="input-container"
+      style={{ background: "#D7EDDD", border: "2px solid #47BA68" }}
+    >
+      <div>
+        <div
+          className="tag"
+          style={{
+            background: "#47BA68",
+            border: "#47BA68 2px solid",
+          }}
+        >
+          Recommended
+        </div>
+        <Checkbox
+          color="green"
+          className="checkbox"
+          label="12 Months Subscription"
+          radius="xl"
+          size="md"
+          fw={"500"}
+        />
+      </div>
+      <div className="text-container">
+        <span>Total ₹179</span>
+        <span> ₹15 /mo</span>
+      </div>
+    </div>
+    <div className="input-container">
+      <Checkbox
+        className="checkbox"
+        label="6 Months Subscription"
+        radius="xl"
+        size="md"
+        fw={"500"}
+      />
+      <div className="text-container">
+        <span>Total ₹179</span>
+        <span> ₹15 /mo</span>
+      </div>
+    </div>
+    <div className="input-container">
+      <Checkbox
+        className="checkbox"
+        label="3 Months Subscription"
+        radius="xl"
+        size="md"
+        fw={"500"}
+      />
+      <div className="text-container">
+        <span style={{ display: "block" }}>Total ₹179</span>
+        <span> ₹15 /mo</span>
+      </div>
+    </div> */}
+        <hr style={{ marginTop: "1.5rem" }} />
+        <div className="summary-container">
+          <div className="st-container">
+            <p>Subscription Fee</p>
+            <p style={{ fontWeight: "600" }}>₹{subscriptionPrice}</p>
+          </div>
+          <div className="alert">
+            <div className="offer">
+              <p>Limited time offer</p>
+              <p style={{ fontWeight: "600" }}>- ₹{discount_price}</p>
+            </div>
+            <div className="alert-body">
+              <img src={NoTime} alt="search-icon"></img>
+              <p style={{ paddingLeft: "5px" }}>
+                Offer valid till 25th March 2023{" "}
+              </p>
+            </div>
+          </div>
+          <div className="total-price">
+            <p>Total (Incl. of 18% GST)</p>
+            <p style={{ fontWeight: "600", fontSize: "1.5rem" }}>
+              ₹{subscriptionPrice ? subscriptionPrice - discount_price : null}
+            </p>
+          </div>
+          <div className="btn-container">
+            <Button
+              style={{ borderWidth: "2px" }}
+              variant="outline"
+              color="red"
+              size="md"
+              w="48%"
+              h="3rem"
+            >
+              CANCEL
+            </Button>
+            <Button size="md" w="48%" h="3rem" color="green">
+              PROCEED TO PAY{" "}
+            </Button>
+          </div>
+          <div style={{ display: "flex", marginTop: "1.2rem" }}>
+            <img src={Razorpay} alt="razorpay"></img>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
